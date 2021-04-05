664a705c85f5d8d5a01cd9db4fea79d6ef98e06b
HosseinTp <www.manotohossein@gmail.com>
2021-04-05 07:34:47 +0430

    setuping Redux

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
 import Reducer from "./Reducer";
 
-const Store = createStore(Reducer);
+const Store = createStore(() => ({
+  tasks: [{ title: "Meet Jonh", date: "Today" }],
+}));
 
 export default Store;
